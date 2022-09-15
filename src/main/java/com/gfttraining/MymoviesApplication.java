ef76bd16839e2f7897490a334b7a76e4f13abce7
Lorenzo <lruizalfieri@gmail.com>
2022-09-15 09:12:04 +0000

    add httpclient dependency to pom, create method to add CORS mappings to a path in MymoviesApplication

--- src/main/java/com/gfttraining/MymoviesApplication.java
+++ src/main/java/com/gfttraining/MymoviesApplication.java
@@ -2,6 +2,9 @@ package com.gfttraining;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MymoviesApplication {
@@ -10,4 +13,13 @@ public class MymoviesApplication {
 		SpringApplication.run(MymoviesApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/movie/*").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
 }
